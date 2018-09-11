b22a4d6ed080d2e5923dac454a1495644990cb03
nilsJavaNoob <nils1968@gmail.com>
2018-09-11 22:07:57 +0400

    added from_drop

---
+++ Floor.java
@@ -0,0 +1,25 @@
+public class Floor {
+	 private int number;
+	 private Appartment [] appartments;
+	 
+	public Floor(int number,  int appartmCount){
+		this.number = number;
+		this.appartments = new Appartment[appartmCount];
+		for(int i = 0;  i< appartmCount;  i++;){
+			appartments[i] = new Appartment();
+		}
+	}
+	 public Appartment getFreeAppartment(){
+		return null;
+	}
+	public String toString(){
+		String result+="=========================\n";
+		 result +="Floor : " + number + "\n " ;
+		result+="----------------------------------------\n";
+		for(Appartment appartment : appartments){
+			result += appartment.toString() + "\n";
+		}
+		result+="=========================\n";
+		return result;
+	}
+}//class
