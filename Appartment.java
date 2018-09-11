ea097cc47f3e5b82ed799c30ead5f6a3ac9e56c9
nilsJavaNoob <nils1968@gmail.com>
2018-09-11 21:40:59 +0400

    first

---
+++ Appartment.java
@@ -0,0 +1,16 @@
+public class Appartment {
+	private String number;
+	
+	public Appartment(String number){
+		this.number = number;
+	}
+	
+	addOwner(Owner owner){
+		//TODO
+	}
+	
+public String toString(){
+	return "Appartment number" + number;
+}	
+}//class
+	
