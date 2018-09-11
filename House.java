ff3c479083908c7edeee404f586298622d70aacc
nilsJavaNoob <nils1968@gmail.com>
2018-09-11 22:18:14 +0400

    added from_drop

--- House.java
+++ House.java
@@ -1,27 +1,27 @@
 public class House {
   private Floor[] floors;
-  
+   
   public House(int count ) {
-	floors = new Floor[count];
-	for(int i = 0;  i< count ; i++) {
-		floors[i] = new Floor(i+1);
-	}
+    floors = new Floor[count];
+    for(int i = 0;  i< count ; i++) {
+        floors[i] = new Floor(i+1);
+    }
   }
   public void settle(Owner owner){
-	  for(Floor floor : floors){
-		 Appartment appartment new = floor.getFreeAppartment();
-		 if(appartment != null){
-			 appartment.addOwner(owner);
-			 breake;
-		 }
-		
-	 }
+      for(Floor floor : floors){
+         Appartment appartment new = floor.getFreeAppartment();
+         if(appartment != null){
+             appartment.addOwner(owner);
+             breake;
+         }
+         
+     }
   }
   public String toString(){
-	  String result = "House :  \n";
-	  for(Floor floor : floors){
-		  result += floor.toString() + "\n";
-	  }
-	  return result;
+      String result = "House :  \n";
+      for(Floor floor : floors){
+          result += floor.toString() + "\n";
+      }
+      return result;
   }
 }//class
